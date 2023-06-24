2a9c2fbc5dfb94b681048572534206066b070998
moerayo <moronfolu.motunrayo1@gmail.com>
2023-06-24 14:49:22 +0100

    feat: phone bill calculator

--- test-automation-uni/exercises/src/chapter5/Exercise.java
+++ test-automation-uni/exercises/src/chapter5/Exercise.java
@@ -3,30 +3,42 @@ package chapter5;
 import java.util.Scanner;
 
 public class Exercise {
+    static double overageCharge = 0.25;
+    static double taxRate = 0.15;
+
     public static void main(String args[]){
         Scanner scanner  = new Scanner(System.in);
+
         System.out.println("Enter base cost of the plan");
-        Double planFee = scanner.nextDouble();
+        double planFee = scanner.nextDouble();
 
         System.out.println("Enter overage minutes:");
-        Double overageMinutes = scanner.nextDouble();
-        Double overageFee = 0.0;
-        Double subTotal = 0.00;
-        Double tax = 0.15;
-        double calculatedTax = 0.0;
-        Double finalTotal;
-        if (overageMinutes > 0){
-            overageFee = overageMinutes * 0.25;
-        }
+        double overageMinutes = scanner.nextDouble();
+        scanner.close();
+
+        double calculatedOverageFee = calculateOverageFee(overageMinutes);
+
+        double tax = calculateTax(planFee, calculatedOverageFee, taxRate);
+
+        double finalTotal = finalTotal(planFee, calculatedOverageFee, tax);
 
 
-        subTotal = planFee + overageFee;
-        calculatedTax = subTotal * tax;
-        finalTotal =  planFee + overageFee + calculatedTax;
         System.out.println("Phone Bill Statement");
         System.out.println("Plan: $"+planFee);
-        System.out.println("Overage: $"+overageFee);
-        System.out.println("Overage: $"+calculatedTax);
-        System.out.println("Overage: $"+finalTotal);
+        System.out.println("Overage: $"+calculatedOverageFee);
+        System.out.println("Tax: $"+tax);
+        System.out.println("total: $"+finalTotal);
+    }
+
+    public static double calculateOverageFee (double overageMinutes){
+        return overageMinutes > 0 ? overageMinutes * overageCharge : 0;
+    }
+
+    public static double calculateTax(double planFee, double calculatedOverageFee, double taxRate){
+        return (planFee + calculatedOverageFee) * taxRate;
+    }
+
+    public static double finalTotal(double planFee, double calculatedOverageFee, double  newTax){
+        return planFee + calculatedOverageFee + newTax;
     }
 }
